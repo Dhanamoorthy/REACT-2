e97d0faaa29dc9ccf2ad1418ba275f810ae89258
Dhanamoorthy <dhanasekar9540@gmail.com>
2024-06-29 13:21:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
-function App() {
+export default function app (){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Data />
     </div>
-  );
+  )
 }
 
-export default App;
+function Data(){
+  const data = [
+    {
+      name : "FREE",
+      price: 0,
+      enjoyments : ["single user",
+    "5GB storage",
+  "unlimited public projects",
+"community access",
+"unlimited private projects",
+"dedicated phone support",
+"Free Subdomain",
+"monthly status reports",
+    ]
+    },
+    {
+      name : "PLUS",
+      price: 9,
+      enjoyments : ["5 users",
+    "50GB storage",
+  "unlimited public projects",
+"community access",
+"unlimited private projects",
+"dedicated phone support",
+"Free Subdomain",
+"monthly status reports",
+    ]
+    },
+    {
+      name : "PRO",
+      price: 49,
+      enjoyments : ["Unlimited users",
+    "150 GB storage",
+  "unlimited public projects",
+"community access",
+"unlimited private projects",
+"dedicated phone support",
+"Unlimited Free Subdomain",
+"monthly status reports",
+    ]
+    }
+  ]
+return (
+  <div className="App">
+    {
+    data.map((plans,index)=>(<Display key = {index} name ={plans.name} price ={plans.price} enjoyments = {plans.enjoyments} />
+    ))};
+  </div>
+
+);
+
+}
+function Display (props){
+  console.log(props);
+  return (
+    <div className = "card">
+    <div className = "plan-name">{props.name}</div>
+    <p>${props.price}<sub>/month</sub></p>
+    <p className='features'>{props.enjoyments.map((e,index) => <li key={index} > {e}</li>)}</p>
+    <button className="buy">Button</button>
+
+    </div>
+
+  );
+}
